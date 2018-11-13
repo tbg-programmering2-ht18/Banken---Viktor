08a296f45185159aa0f807654fcf7d89942fe6f3
WilfordWonChuckleberg <gintokisakatathesamurai@gmail.com>
2018-11-13 11:50:32 +0100

    xd

--- Banken/Program.cs
+++ Banken/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace banken
 {
@@ -12,7 +13,7 @@ namespace banken
 
         static void Main(string[] args)
         {
-            
+            //skriver ut dom olika valen i banken som ska hända.
             Console.WriteLine("Vällkommen till banken!");
             Console.WriteLine("");
             Console.WriteLine("Ange vilket av följande alternativ önskar dugöra.");
@@ -27,10 +28,12 @@ namespace banken
             Console.WriteLine("");
             Console.WriteLine("Skriv ditt val: ");
 
+            int val = 0;
+                
             string valStr = Console.ReadLine();
             try
             {
-                int val = int.Parse(valStr); //test
+               val = int.Parse(valStr); //test
             }
             catch(FormatException e)
             {
@@ -43,15 +46,16 @@ namespace banken
                 switch (val)
                 {
                     case 1:
-                        AddCustomer();
+                        AddCustomer(); //Lägger till en användare i programmet 
                         break;
                     case 2:
-                        RemoveCustomer();
+                        RemoveCustomer();//Tar bort en användare från programmet
                         break;
                     case 3:
                         foreach (customer info in CustomerList)
                         {
-                            Console.WriteLine(info.CustomerInfo());
+                            Console.WriteLine(info.CustomerInfo());//visar alla användarna man har skrivit in
+                            string.mydoc // filhantering
                         }
 
                         break;
@@ -72,7 +76,7 @@ namespace banken
                         Console.WriteLine("Default case");
                         break;
                 }
-                Console.WriteLine("Skriv ditt val: ");
+                Console.WriteLine("Skriv ditt val: ");// Tar in det  du skriver som val  
                 valStr = Console.ReadLine();
                 val = int.Parse(valStr);
 
@@ -92,14 +96,14 @@ namespace banken
 
         public static void RemoveCustomer() // Tar bort en Customer från listan
         {
-            Console.WriteLine("write the name of the customer: ");
+            Console.WriteLine("write the name of the customer: "); //skriver ut Namnet på en användare som ska tas bort
             string Customertodelete = Console.ReadLine();
             customer objecttodelete = null;
             foreach (customer info in CustomerList)
             {
                 if (info.Namn == Customertodelete)
                     objecttodelete = info;
-                Console.WriteLine(" Du tog bort " + info.CustomerInfo());
+                Console.WriteLine(" Du tog bort " + info.CustomerInfo()); //skriver ut personen som du tog bort
                
             }
             CustomerList.Remove(objecttodelete);
